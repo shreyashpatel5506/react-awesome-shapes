9113f0854d70b3095174239ee41fd166da1eb45b
Shreyash Patel <160259841+shreyashpatel5506@users.noreply.github.com>
2025-12-23 04:44:19 +0000

    fix: resolve theme-ui export and prettier formatting

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { Themed, Box } from 'theme-ui';
+import { Link, Box } from 'theme-ui';
 import { discordLink, githubLink } from '../../constants/social.constant';
 
 export const Footer = () => {
@@ -9,11 +9,16 @@ export const Footer = () => {
   return (
     <AnimatedBox as="footer" variant="layout.footer">
       &copy; {new Date().getFullYear()} by{` `}
-      <Themed.a href={discordLink}>DevSense</Themed.a>
+      <Link href={discordLink} rel="noopener noreferrer" target="_blank">
+        DevSense
+      </Link>
       . All rights reserved.
       <br />
       This website is open source on{` `}
-      <Themed.a href={githubLink}>GitHub</Themed.a>.
+      <Link href={githubLink} rel="noopener noreferrer" target="_blank">
+        GitHub
+      </Link>
+      .
     </AnimatedBox>
   );
 };
